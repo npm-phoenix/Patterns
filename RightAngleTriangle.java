2ab52b56f03b0f6a20663a4db5dc3e0a6400b096
Rishi Jha <126956744+Jha-2022@users.noreply.github.com>
2025-02-21 00:37:02 +0530

    Update RightAngleTriangle.java

--- RightAngleTriangle.java
+++ RightAngleTriangle.java
@@ -1,1 +1,12 @@
+// Right angle triangle throgh *s
 
+public class RightTriangle{
+  public static void tightAngleTriangleF3orest(int n){
+    for(int i=0;i<n;i++){
+      for(int j=0;j<=i;j++){
+            System.out.print("* ");        
+      }
+      System.out.println("");
+    }
+  }
+}
